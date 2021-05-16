554d7292af3bc72d8926b36dbf95d284ac105ec4
pqvick <65366297+pqvick@users.noreply.github.com>
2021-05-16 13:25:27 -0300

    JS

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){ //condição simples!
+    console.log('voce ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('diriga sempre usando cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'brasil'
+console.log(`Vivendo em ${país}`)
+if(país == 'brasil') {
+    console.log('você é brasileiro!')
+}  else {
+    console.log('você é estrangeiro!')
+}
