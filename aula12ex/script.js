2c82dcaa632c5c110262f6deb11f5cab629bc17b
pqvick <65366297+pqvick@users.noreply.github.com>
2021-07-16 13:23:44 -0300

    Co-authored-by: Gabriel Rodrigues <Apenasgabs@users.noreply.github.com>

--- aula12ex/script.js
+++ aula12ex/script.js
@@ -1,1 +1,24 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('foto')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12) {
+        console.log('1')
+        //BOM DIA!
+        img.innerHTML = '<img src="fotomanha.png">' 
+        document.body.style.background = '#e2cd9f'
+    }else if (hora >= 12 && hora <= 18) {
+        console.log('2')
+        //BOA TARDE!
+        
+        img.innerHTML = '<img src="fototarde.png" alt="Foto do dia">'
+        document.body.style.background = '#b9846f'
+    }else {
+        console.log('3')
+        //BOA NOITE!
+        img.innerHTML = '<img src="fotonoite.png">'
+        document.body.style.background = '#515154'
+    }
+}
